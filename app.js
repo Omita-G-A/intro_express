4b9622882464de8716ab53d1fa8703c88e1d5765
Omi Gonzalez <omienred@gmail.com>
2021-12-14 15:01:07 +0100

    segundo commit

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 const express = require('express');
+
+const app = express(); // ejecutamos la funcion express que devolverá un objeto
+
+const PORT = process.env.PORT || 5001; // iniciamos la constante
+// port de la manera que demanda heroku en vistas de una posible subida
+// de la app en esa plataforma.
+
+app.get('/', (req, res) => {
+    res.send('Holaaaa!')
+});
+
+app.listen(PORT, () => {
+    console.log('Express escuchando en puerto: ' + PORT);
+})
+
